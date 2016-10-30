eb4991d3e4a215411a7be8f0c2cac790661ad172
Jack Borthwick <jack.borthwick@coloradocollege.edu>
2016-10-30 18:29:20 +0000

    model built

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,11 +7,55 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :guesses
+  attr_accessor :word
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = '--------------------------------------'[0..word.length-1]
   end
-
+  
+  def guess(l) 
+    if l == nil || l == '' || l[/[a-zA-Z]+/]  != l
+      raise (ArgumentError)
+    end
+    if (@word.downcase.include? l.downcase) 
+      if @guesses.include? l.downcase
+        return false
+      end
+      @guesses = @guesses + l.downcase
+      for i in 0...@word.length
+        if l == @word[i]
+          @word_with_guesses[i] = l
+        end
+      end
+      return true
+    elsif @wrong_guesses.downcase.include? l.downcase 
+      return false
+    else
+      @wrong_guesses = @wrong_guesses + l.downcase
+      return true
+    end
+    check_win_or_lose
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length == 7
+      return :lose
+    else
+      for i in 0...@word.length
+        if !@guesses.include? @word[i]
+          return :play
+        end
+      end
+      return :win
+    end
+  end
+      
   def self.get_random_word
     require 'uri'
     require 'net/http'
